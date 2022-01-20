7aadb69a0db1823442f5e9eea5329d4c5b261549
gaotao <646101390@qq.com>
2022-01-21 01:27:29 +0800

    作用域学习

---
+++ 01_learn_scope/03__全局代码执行过程(函数).js
@@ -0,0 +1,24 @@
+var name = "why";
+
+foo(123)
+function foo(num) {
+  console.log(m);
+  var m = 10
+  var n = 20
+  console.log("foo");
+
+}
+
+
+var num1 = 20 
+var num2 = 30
+var result = num1 + num2
+
+
+// 执行过程
+
+var GlobalObject = {
+  String: "类",
+  window: GlobalObject,
+  name: undefined
+}
---
+++ 01_learn_scope/04__全局代码执行过程(函数嵌套).js
@@ -0,0 +1,27 @@
+var name = "why";
+
+foo(123)
+function foo(num) {
+  console.log(m);
+  var m = 10
+  var n = 20
+  function bar () {
+    console.log(name);
+  }
+
+  bar()
+}
+
+
+var num1 = 20 
+var num2 = 30
+var result = num1 + num2
+
+
+// 执行过程
+
+var GlobalObject = {
+  String: "类",
+  window: GlobalObject,
+  name: undefined
+}
